afbf36d4386d0f6aa90ab218c03865f596be7fc7
Fabio Martineli <fabio.azure.devops@gmail.com>
2024-08-14 22:52:05 -0300

    Add project files.

---
+++ App/Program.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Polly;
+using Polly.CircuitBreaker;
+using Polly.Retry;
+using Polly.Fallback;
+
+
+var builder = Host.CreateApplicationBuilder(args);
+
+builder.Services.AddResiliencePipeline<string, string>("retry", opt =>
+{
+    opt.AddRetry(new RetryStrategyOptions<string>
+    {
+        Name = "Retry",
+        BackoffType = DelayBackoffType.Constant,
+        Delay = TimeSpan.FromSeconds(5),
+        MaxRetryAttempts = 10,
+        ShouldHandle = new PredicateBuilder<string>().HandleResult(result => result is not "true"),
+        OnRetry = result =>
+        {
+            Console.WriteLine(result.Outcome.Result);
+            return new ValueTask();
+        },
+    })
+   .AddFallback(new FallbackStrategyOptions<string>
+   {
+       FallbackAction = result => new ValueTask<Outcome<string>>(Outcome.FromResult("true")),
+       Name = "Fallback"
+   })
+    .AddCircuitBreaker(new CircuitBreakerStrategyOptions<string>
+    {
+        FailureRatio = 0, // PERCENTUAL DE FALHAS PARA SER CONSIDERADO NO CIRCUITO
+        BreakDuration = TimeSpan.FromSeconds(1),
+        Name = "Circuit Breaker Retry",
+        ShouldHandle = new PredicateBuilder<string>().HandleResult(result => result is not "true"),
+        MinimumThroughput = 9, // QUANTIDADE DE ERRO DE EXECUCAO PARA ATIVAR O CIRCUITO
+    })
+
+    ;
+
+});
+builder.Services.AddSingleton<RetryService>();
+
+var host = builder.Build();
+((RetryService)host.Services.GetRequiredService(typeof(RetryService))).Execute();
---
+++ App/RetryService.cs
@@ -0,0 +1,36 @@
+﻿using Polly.Registry;
+
+public class RetryService
+{
+    private static int COUNT01 = 0;
+    private static int COUNT02 = 0;
+    private readonly ResiliencePipelineProvider<string> _pipelineResilience;
+
+    public RetryService(ResiliencePipelineProvider<string> pipelineResilience)
+    {
+        _pipelineResilience = pipelineResilience;
+    }
+
+    public void Execute()
+    {
+        var pipeline = _pipelineResilience.GetPipeline<string>("retry");
+        Parallel.Invoke
+        (
+            () => pipeline.Execute(() =>
+            {
+                COUNT01++;
+                if (COUNT01 == 9) return "true"; // FINALIZA O PROCESSO COM SUCESSO
+                Console.WriteLine("Execução 01 - COUNTER: " + COUNT01); 
+                return "Execução 01"; // RETORNA STRING INVALIDA PARA REPROCESSAR NOVAMENTE
+            })
+            ,
+            () => pipeline.Execute(() =>
+            {
+                COUNT02++;
+                if (COUNT02 == 3) return "true"; // FINALIZA O PROCESSO COM SUCESSO
+                Console.WriteLine("Execução 02 - COUNTER: " + COUNT02);
+                return "Execução 02"; // RETORNA STRING INVALIDA PARA REPROCESSAR NOVAMENTE
+            })
+        );
+    }
+}
